a17fb583963e53c4c5e89bea484a59c7e69a6f80
Nilseu Padilha <ragnarokkrr@gmail.com>
2014-07-14 23:05:03 -0300

    upgrade scala 2.11.1

--- rgn_thinkcomp_python/src/main/jython/org/ragna/thinkcomp/test1.py
+++ rgn_thinkcomp_python/src/main/jython/org/ragna/thinkcomp/test1.py
@@ -6,13 +6,4 @@ Created on 14/05/2013
 
 if __name__ == '__main__':
     print "AAAAAA"
-    
-    
-#assertNotEqual(a,b) : The opposite of assertEqual().
-#assertAlmostEqual(a, b): Only used for numeric comparison. It adds a sort of tolerance for insignificant differences by subtracting its first two arguments after rounding them to the seventh decimal place, and later comparing the resnult to zero. You can specify a different number of decimal places in the third argument. This is useful for comparing floating point numbers.
-#assertNotAlmostEqual(a, b): The opposite of assertAlmostEqual().
-#assert_(x): Accepts a Boolean argument, expecting it to be . You can use it to write other checks, such as “greater than,” or to check Boolean functions/attributes. (The trailing underscore is needed because is a keyword.)
-#assertFalse(x). The opposite of assert_().
-#assertRaises(exception, callable). Used to assert that an exception passed as the first argument is thrown when invoking the callable specified as the second argument. The rest of the arguments passed to are passed on to the callable.
- 
-#
+
